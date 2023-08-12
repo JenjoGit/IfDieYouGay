96b54b81f155e294cbe7fe0f6e839f5529324868
Jens Kulka <kulkajens82@gmail.com>
2023-08-13 01:01:31 +0200

    Added Enmeies so that they spawn randomly around the player and outside the viewport

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -4,25 +4,42 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    [SerializeField] private GameObject prefab; 
+    [SerializeField] private GameObject prefab;
+    public float minSpawnInterval = 2f;
+    public float maxSpawnInterval = 2f;
+    public Transform player;
+    [SerializeField] float distance = 100f;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello");
-        // for(int i = 0; i <= 10; i++)
-        // {
-        //     Instantiate(prefab, new Vector2(i, i), Quaternion.identity);
-        // }
-        
+        StartCoroutine(SpawnEnemies());        
     }
     // Update is called once per frame Dependent on Framerate = Bad 
     void Update()
     {
 
     }
-    //Called  50/sec by Default not tied to fps = good
-    void FixedUpdate()
+    private IEnumerator SpawnEnemies()
     {
+        while (true)
+        {
+            float spawnInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+            yield return new WaitForSeconds(spawnInterval);
 
+            SpawnEnemy();
+        }
+    }
+    void SpawnEnemy()
+    {
+        Vector3 spawnPosition = CalculateSpawnPosition();
+        Instantiate(prefab, spawnPosition, Quaternion.identity);
+    }
+
+    Vector3 CalculateSpawnPosition()
+    {
+        float angle = Random.Range(0f, 360f);
+        Vector3 offset = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0f) * distance;
+        Vector3 spawnPos = player.position + offset;
+        return spawnPos;
     }
 }
