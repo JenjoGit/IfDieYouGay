07ca12a012028365294478cb6ad893c60507214c
Jens Kulka <kulkajens82@gmail.com>
2023-08-19 03:25:29 +0200

    Chore: Fixed some errors

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
